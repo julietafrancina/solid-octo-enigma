2a70c8be9265f404ad521e14c66bb0501ca03734
Leonel Rodriguez <leorodriguez_01@hotmail.com>
2024-05-08 11:25:13 -0300

    Modificaciones relacionadas a la conexión con la base de datos

---
+++ CapaDatos/CD_Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+
+namespace CapaDatos
+{
+    public class CD_Usuario
+    {
+
+        public List<Usuario> listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "select id_usuario, dni, nombre_completo, correo, contraseña from Usuario";
+
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+
+                }
+                catch(Exception ex)
+                {
+
+                }
+
+
+
+            }
+
+
+        }
+
+    }
+}
