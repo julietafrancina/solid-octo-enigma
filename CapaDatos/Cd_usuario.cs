fbe891ab330f1b4c967f6daffb91917c35e451a1
lucia cura <lucia.cura97@gmail.com>
2024-05-09 09:01:22 -0300

    cambios guardados

---
+++ CapaDatos/Cd_usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+
+namespace CapaDatos
+{
+    public class Cd_usuario{
+
+        public List<Usuario> Listar() {
+
+            List<Usuario> lista = new List<Usuario>(),
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try {
+
+                string query = "select idUsuario, dni, nombreCompleto, correo, contraseña from Usuario";
+                SqlCommand cmd = new SqlCommand(query, oconexion);
+                cmd.CommandType = CommandType.Text;
+
+                oconexion.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader()) {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Usuario() {
+
+                            idUsuario = Convert.ToInt32(dr["idUsuario"]),
+                            dni = dr["dni"].ToString(),
+                            nombreCompleto = dr["nombreCompleto"].ToString(),
+                            correo = dr["correo"].ToString(),
+                            contraseña = dr["contraseña"].ToString(),
+
+                        });
+                    }
+                }
+            } catch (Exception ex) {
+                lista = new List<Usuario>();
+            }
+        }
+        return lista;    
+
+        }
+
+        }
+    }
+
---
+++ CapaNegocio/Cn_Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+ public class Cn_Usuario{
+
+        private Cd_usuario objcd_usuario = new Cd_usuario();
+        public List<Usuario> Listar()
+        {
+            return objcd_usuario.Listar();
+
+        }
+
+
+
+
+    }
+}
