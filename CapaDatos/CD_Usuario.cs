fca12a76595d98744eee6ca9ac38a53271704b8b
Julieta Visser <julietafvisser@gmail.com>
2024-05-09 09:07:52 -0300

    cuestiones de conexión con la base

---
+++ CapaDatos/CD_Usuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaEntidad;
+
+namespace CapaDatos
+{
+    public class CD_Usuario
+    {
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    string query = "SELECT idUsuario, dni, nombreCompleto, correo, contraseña FROM Usuario";
+                    SqlCommand cmd = new SqlCommand(query, oconexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Usuario()
+                            {
+                                idUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                dni = dr["dni"].ToString(),
+                            }
+                                );
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    // Esta clase es la que envía a las demás clases la cadena de conexión que tenemos almacenada en App.config. 
+    public class Conexion
+    {
+        public static string cadena = ConfigurationManager.ConnectionStrings["cadena_conexion"].ToString();
+    }
+}
