02650d0c1b2bbce02b79d350eeb2d74c532fc814
Leonel Rodriguez <leorodriguez_01@hotmail.com>
2024-05-30 11:06:55 -0300

    Commit prueba

--- CapaPresentacion/form_Clientes.cs
+++ CapaPresentacion/form_Clientes.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CapaEntidad;
 using CapaNegocio;
-using CapaPresentacion.Utilidades;
 
 namespace CapaPresentacion
 {
@@ -22,21 +21,6 @@ namespace CapaPresentacion
 
         private void form_Clientes_Load(object sender, EventArgs e)
         {
-
-            foreach (DataGridViewColumn columna in dgvData.Columns)
-            {
-
-                if (columna.Visible == true && columna.Name != "btnSeleccionar")
-                {
-                    cboBusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
-                }
-
-            }
-            cboBusqueda.DisplayMember = "Texto";
-            cboBusqueda.ValueMember = "Value";
-            cboBusqueda.SelectedIndex = 0;
-
-
             List<Cliente> listaCliente = new CN_Cliente().listar();
 
             foreach (Cliente item in listaCliente)
@@ -115,40 +99,29 @@ namespace CapaPresentacion
         private void bntGuardar_Click(object sender, EventArgs e)
         {
 
-            if (txtDNI.Text == "")
+            string mensaje = string.Empty;
+
+            string fecha = txtDia.Text + "/" + txtMes.Text + "/" + txtAnio.Text;
+
+            Cliente obj = new Cliente()
             {
-                MessageBox.Show("Es necesario el documento del Cliente.");
-            }
-            else if (txtDia.Text == "dd" || txtMes.Text == "mm" || txtAnio.Text == "yyyy")
-            {
-                MessageBox.Show("Es necesaria la fecha de nacimiento del Cliente.");
-            }
-            else
+                idCliente = Convert.ToInt32(txtId.Text),
+                dni = Convert.ToInt32(txtDNI.Text),
+                nombreCompleto = txtNombreCompleto.Text,
+                correo = txtCorreo.Text,
+                telefono = txtTelefono.Text,
+                domicilio = txtDomicilio.Text,
+                fechaNacimiento = Convert.ToDateTime(fecha)
+            };
+
+            if (obj.idCliente == 0)
             {
 
-                string mensaje = string.Empty;
+                int idgenerado = new CN_Cliente().Registrar(obj, out mensaje);
 
-                string fecha = txtDia.Text + "/" + txtMes.Text + "/" + txtAnio.Text;
-
-                Cliente obj = new Cliente()
+                if (idgenerado != 0)
                 {
-                    idCliente = Convert.ToInt32(txtId.Text),
-                    dni = Convert.ToInt32(txtDNI.Text),
-                    nombreCompleto = txtNombreCompleto.Text,
-                    correo = txtCorreo.Text,
-                    telefono = txtTelefono.Text,
-                    domicilio = txtDomicilio.Text,
-                    fechaNacimiento = Convert.ToDateTime(fecha)
-                };
-
-                if (obj.idCliente == 0)
-                {
-
-                    int idgenerado = new CN_Cliente().Registrar(obj, out mensaje);
-
-                    if (idgenerado != 0)
-                    {
-                        dgvData.Rows.Add(new object[] {
+                    dgvData.Rows.Add(new object[] {
                         "",
                         idgenerado,
                         txtDNI.Text,
@@ -159,38 +132,17 @@ namespace CapaPresentacion
                         fecha
                     });
 
-                        limpiar();
-                    }
-                    else
-                    {
-                        MessageBox.Show(mensaje);
-                    }
+                    limpiar();
                 }
                 else
                 {
-                    bool resultado = new CN_Cliente().Editar(obj, out mensaje);
-
-                    if (resultado)
-                    {
-                        DataGridViewRow row = dgvData.Rows[Convert.ToInt32(txtIndice.Text)];
-                        row.Cells["Id"].Value = txtId.Text;
-                        row.Cells["DNI"].Value = txtDNI.Text;
-                        row.Cells["NombreCompleto"].Value = txtNombreCompleto.Text;
-                        row.Cells["Correo"].Value = txtCorreo.Text;
-                        row.Cells["Telefono"].Value = txtTelefono.Text;
-                        row.Cells["Domicilio"].Value = txtDomicilio.Text;
-                        row.Cells["FechaNacimiento"].Value = fecha;
-                        limpiar();
-                    }
-                    else
-                    {
-                        MessageBox.Show(mensaje);
-                    }
+                    MessageBox.Show(mensaje);
                 }
-
-
             }
-            
+            else
+            {
+                MessageBox.Show("No se puede guardar nuevamente un cliente que ya existe.");
+            }
             
         }
 
@@ -209,15 +161,63 @@ namespace CapaPresentacion
             txtAnio.Text = "";
         }
 
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            string mensaje = string.Empty;
+
+            string fecha = txtDia.Text + "/" + txtMes.Text + "/" + txtAnio.Text;
+
+            Cliente obj = new Cliente()
+            {
+                idCliente = Convert.ToInt32(txtId.Text),
+                dni = Convert.ToInt32(txtDNI.Text),
+                nombreCompleto = txtNombreCompleto.Text,
+                correo = txtCorreo.Text,
+                telefono = txtTelefono.Text,
+                domicilio = txtDomicilio.Text,
+                fechaNacimiento = Convert.ToDateTime(fecha)
+            };
+
+            if (obj.idCliente != 0)
+            {
+
+                bool resultado = new CN_Cliente().Editar(obj, out mensaje);
+
+                if (resultado)
+                {
+                    DataGridViewRow row = dgvData.Rows[Convert.ToInt32(txtIndice.Text)];
+                    row.Cells["Id"].Value = txtId.Text;
+                    row.Cells["DNI"].Value = txtDNI.Text;
+                    row.Cells["NombreCompleto"].Value = txtNombreCompleto.Text;
+                    row.Cells["Correo"].Value = txtCorreo.Text;
+                    row.Cells["Telefono"].Value = txtTelefono.Text;
+                    row.Cells["FechaNacimiento"].Value = fecha;
+                    limpiar();
+                }
+                else
+                {
+                    MessageBox.Show(mensaje);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("No se puede editar un cliente que no existe.");
+            }
+        }
 
         private void txtDNI_TextChanged(object sender, EventArgs e)
         {
-            
+            if (!System.Text.RegularExpressions.Regex.IsMatch(txtDNI.Text, "^[0-9]*$"))
+            {
+                txtDNI.Text = string.Empty;
+                MessageBox.Show("Solo ingrese números por favor.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void txtDia_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         private void txtDomicilio_TextChanged(object sender, EventArgs e)
@@ -227,7 +227,7 @@ namespace CapaPresentacion
 
         private void txtTelefono_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
 
@@ -296,7 +296,7 @@ namespace CapaPresentacion
 
             if(Convert.ToInt32(txtId.Text) != 0)
             {
-                if(MessageBox.Show("¿Desea eliminar el cliente?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                if(MessageBox.Show("¿Desea eliminar el cliente", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
 
                     string mensaje = string.Empty;
@@ -320,111 +320,6 @@ namespace CapaPresentacion
                 }
             }
 
-        }
-
-        private void btnLimpiar_Click(object sender, EventArgs e)
-        {
-            txtBusqueda.Text = "";
-            foreach(DataGridViewRow row in dgvData.Rows)
-            {
-                row.Visible = true;
-            }
-        }
-
-        private void btnBusqueda_Click(object sender, EventArgs e)
-        {
-            string columnaFiltro = ((OpcionCombo)cboBusqueda.SelectedItem).Valor.ToString();
-
-            if(dgvData.Rows.Count > 0)
-            {
-                foreach(DataGridViewRow row in dgvData.Rows)
-                {
-                    if (row.Cells[columnaFiltro].Value.ToString().Trim().ToUpper().Contains(txtBusqueda.Text.Trim().ToUpper()))
-                        row.Visible = true;
-                    else
-                        row.Visible = false;
-                }
-            }
-        }
-
-        private void txtMes_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtAnio_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void cboBusqueda_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnEraser_Click(object sender, EventArgs e)
-        {
-            txtDNI.Text = "";
-            txtNombreCompleto.Text = "";
-            txtCorreo.Text = "";
-            txtTelefono.Text = "";
-            txtDomicilio.Text = "";
-            txtAnio.Text = "yyyy";
-            txtAnio.ForeColor = System.Drawing.Color.Gray;
-            txtMes.Text = "mm";
-            txtMes.ForeColor = System.Drawing.Color.Gray;
-            txtDia.Text = "dd";
-            txtDia.ForeColor = System.Drawing.Color.Gray;
-        }
-
-        private void txtDNI_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Verifico si la tecla presionada es un nro o una tecla de control 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Si no es un número ni una tecla de control, cancelar el evento
-                e.Handled = true;
-            }
-        }
-
-        private void txtTelefono_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Verifico si la tecla presionada es un nro o una tecla de control 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Si no es un número ni una tecla de control, cancelar el evento
-                e.Handled = true;
-            }
-        }
-
-        private void txtDia_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Verifico si la tecla presionada es un nro o una tecla de control 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Si no es un número ni una tecla de control, cancelar el evento
-                e.Handled = true;
-            }
-        }
-
-        private void txtMes_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Verifico si la tecla presionada es un nro o una tecla de control 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Si no es un número ni una tecla de control, cancelar el evento
-                e.Handled = true;
-            }
-        }
-
-        private void txtAnio_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            // Verifico si la tecla presionada es un nro o una tecla de control 
-            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
-                // Si no es un número ni una tecla de control, cancelar el evento
-                e.Handled = true;
-            }
         }
     }
 }
